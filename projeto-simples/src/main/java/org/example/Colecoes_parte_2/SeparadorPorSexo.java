d3cff8adfdd079a4bff0937301042717738dc11a
WendelSReis <wendel.m.w18@gmail.com>
2023-09-04 21:59:36 -0300

    Feat: Adicionando while

--- projeto-simples/src/main/java/org/example/Colecoes_parte_2/SeparadorPorSexo.java
+++ projeto-simples/src/main/java/org/example/Colecoes_parte_2/SeparadorPorSexo.java
@@ -9,6 +9,26 @@ public class SeparadorPorSexo {
         List<String> nomesMasculinos = new ArrayList<>();
         List<String> nomesFemininos = new ArrayList<>();
 
+        while (true) {
+            System.out.println("Digite um nome (ou 'sair' para encerrar):");
+            String entrada = scanner.nextLine();
+
+            if (entrada.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            System.out.println("Digite o sexo (M para masculino, F para feminino):");
+            String sexo = scanner.nextLine();
+
+            if (sexo.equalsIgnoreCase("M")) {
+                nomesMasculinos.add(entrada);
+            } else if (sexo.equalsIgnoreCase("F")) {
+                nomesFemininos.add(entrada);
+            } else {
+                System.out.println("Sexo inválido. Use 'M' para masculino ou 'F' para feminino.");
+            }
+        }
+
         scanner.close();
     }
 }
