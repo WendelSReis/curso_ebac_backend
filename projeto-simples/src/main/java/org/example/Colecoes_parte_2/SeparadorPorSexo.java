246ecc37e93a7975d49b5eebdea379e589df0f7b
WendelSReis <wendel.m.w18@gmail.com>
2023-09-04 21:46:07 -0300

    Feat: Exercício Hash

---
+++ projeto-simples/src/main/java/org/example/Colecoes_parte_2/SeparadorPorSexo.java
@@ -0,0 +1,14 @@
+package org.example.Colecoes_parte_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class SeparadorPorSexo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> nomesMasculinos = new ArrayList<>();
+        List<String> nomesFemininos = new ArrayList<>();
+
+        scanner.close();
+    }
+}
